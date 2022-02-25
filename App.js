fc2d2ad2588ab23e34c01fa406df247af653946d
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-25 18:11:32 -0300

    'OI MUNDO

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Hello word!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+    texto:{ 
+      color:'#cccc',
+      FontSize: 30
+
+    },
+
+});
