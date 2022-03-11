962b4981a239c6e9223216f065a37e165353f327
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-11 18:23:18 -0300

    Completo

--- App.js
+++ App.js
@@ -1,11 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
+  const [valor1, setValor1] = useState();
+  const [valor2, setValor2] = useState();
+  const [resultado, setResultado] = useState(0);
+
+  function somar(){
+    setResultado(parseFloat(valor1) + parseFloat(valor2));
+  }
+  function subtrair(){
+    setResultado(parseFloat(valor1) - parseFloat(valor2));
+  }
+  function multiplicar(){
+    setResultado(parseFloat(valor1) * parseFloat(valor2));
+  }
+  function dividir(){
+    setResultado(parseFloat(valor1) / parseFloat(valor2));
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Hello word!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Calculadora</Text>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: 'https://cdn.icon-icons.com/icons2/72/PNG/256/calculator_14445.png',
+        }}
+      />
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Digite o 1 número</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+          value={valor1}
+          onChangeText={(texto)=>setValor1(texto)}
+        />
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>Digite o 2 número</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType="numeric"
+          value={valor2}
+          onChangeText={(texto)=>setValor2(texto)}
+        />
+      </View>
+
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao}
+          onPress={somar}
+        >
+            <Text style={styles.textoBotao}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao}
+          onPress={subtrair}
+        >
+            <Text style={styles.textoBotao}>-</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao}
+          onPress={multiplicar}
+        >
+            <Text style={styles.textoBotao}> x </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+          style={styles.botao}
+          onPress={dividir}
+        >
+            <Text style={styles.textoBotao}>÷</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}>= {resultado}</Text>
+      </View>
     </View>
   );
 }
@@ -13,14 +93,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
-    texto:{ 
-      color:'#cccc',
-      FontSize: 30
-
-    },
-
+  textoBloco:{
+    fontSize:20,
+  },
+  texto:{
+    color:'#000',
+    fontSize:30
+  },
+  input:{
+    borderColor:'#000',
+    borderWidth:2,
+    fontSize:30,
+    width:'80%'
+  },
+  bloco:{
+    width:'100%',
+    alignItems:'center',
+    marginTop:30
+  },
+  botao:{
+    backgroundColor:'#000',
+    width:'80%',
+    textAlign:'center'
+  },
+  textoBotao:{
+    color:"#fff",
+    fontSize:30
+  }, 
+  logo:{
+    width:50,
+    height:50
+  }
 });
